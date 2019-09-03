a9ac5cadd0c77888b20857ae99cccefe366eb0c6
Shaun Foxcroft <sfoxcroft@outlook.com>
2019-09-03 11:00:43 +0100

    Implemented a toggle for the dimming TODO: remember last dim setting for future visits or next episode links

--- crunchyroll.user.js
+++ crunchyroll.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name     Crunchyroll Playback Focus (Script)
-// @version  1.3.0
+// @version  1.4
 // @grant    none
 // @include	 /^https?://www\.crunchyroll.com/.*/.*-[0-9]*.*/
 // @author Shaun Foxcroft <sfoxcroft@outlook.com> (https://github.com/S-Foxcroft)
@@ -20,38 +20,27 @@ function run(){
   for(i=0;i<ea.length;i++){
     ea[i].style = ""; 
   }
+  if(!window.userjs) window.userjs = {};
+  window.userjs.dim = false;
   
   console.log("~ Insertion");
-  var newDivs = [document.createElement("div"),document.createElement("div"),document.createElement("div"),
-                 document.createElement("div")];
+  
   var closeDiv = document.createElement("div");
   var nextDiv = document.createElement("div");
   var prevDiv = document.createElement("div");
   closeDiv.id = "closeDiv";
-  closeDiv.innerText = "X";
+  closeDiv.innerText = "Lights back on";
   nextDiv.id="nextDiv";
   nextDiv.innerText = ">";
   prevDiv.id="prevDiv";
   prevDiv.innerText = "<";
   document.body.appendChild(closeDiv);
   closeDiv.addEventListener('click',function(){
-  	var del = document.getElementsByClassName("dimmerswitch");
-    destroy(del[0]);
-    destroy(del[0]);
-    destroy(del[0]);
-    destroy(del[0]);
-    destroy(document.getElementById("closeDiv"));
-    document.getElementsByTagName("header")[0].className = document.getElementsByTagName("header")[0]
-                                                           .className.replace(" blackheader","");
+	  if(window.userjs.dim) closeBox();
+	  else openBox();
   });
-  newDivs[0].id="dsL";
-  newDivs[1].id="dsR";
-  newDivs[2].id="dsT";
-  newDivs[3].id="dsB";
-  newDivs.forEach(function(elem){
-    elem.className = "dimmerswitch";
-  	document.body.appendChild(elem);
-  });
+  openBox();
+  
   
   console.log("~ Previous and Next - Only required if episode is listed");
   var episodeCollection = document.getElementsByClassName("collection-carousel-media");
@@ -76,7 +65,7 @@ function run(){
     });
   }
   
-	console.log("~ Removal");
+  console.log("~ Removal");
   destroy(document.getElementsByClassName("guestbook")[0]);
   var removed = document.getElementsByClassName("showmedia-related");
   destroy(removed[0]);
@@ -86,4 +75,28 @@ function run(){
 function destroy(elem){
  elem.parentNode.removeChild(elem); 
 }
+function closeBox(){
+	var me = document.getElementById("closeDiv");
+	me.innerText = "Lights off";
+  	var del = document.getElementsByClassName("dimmerswitch");
+    destroy(del[0]);
+    destroy(del[0]);
+    destroy(del[0]);
+    destroy(del[0]);
+    document.getElementsByTagName("header")[0].className = document.getElementsByTagName("header")[0]
+                                                           .className.replace(" blackheader","");
+}
+function openBox(){
+	var newDivs = [document.createElement("div"),document.createElement("div"),document.createElement("div"),
+                 document.createElement("div")];
+	newDivs[0].id="dsL";
+    newDivs[1].id="dsR";
+    newDivs[2].id="dsT";
+    newDivs[3].id="dsB";
+    newDivs.forEach(function(elem){
+        elem.className = "dimmerswitch";
+  	    document.body.appendChild(elem);
+    });
+}
+
 setTimeout(run,75);
